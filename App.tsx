dffdc762bde20553bcf94867fd3a3247bc8cf188
ParvejKhan <parvej.khan@extwebtech.in>
2023-06-26 19:20:12 +0530

    BottomSheetExtended

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+import Home from './components/Home'
+
+
+export class App extends Component {
+  render() {
+    return (
+      <Home/>
+    )
+  }
+}
+
+export default App
